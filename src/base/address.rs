4aa8d28c73882307e61dff68def612f38a8da2e8
ksb <ksb@users.sourceforge.net>
2019-12-03 18:45:22 +0100

    Better addresses.

--- src/base/address.rs
+++ src/base/address.rs
@@ -1,331 +1,271 @@
 use std::convert::TryFrom;
 use std::convert::TryInto;
 
-#[derive(Debug, Copy, Clone)]
-struct BusAddress(u8);
+pub trait BusAddress
+{
+    fn bus_address(&self) -> u8;
+}
 
 #[derive(Debug, Copy, Clone, PartialEq)]
-enum AddressType
+pub enum Address
 {
-    Short(ShortAddress),
-    Group(GroupAddress),
+    Short(Short),
+    Group(Group),
     Broadcast
 }
-    
-impl BusAddress
-{
-    
-    pub fn new(a: u8) ->BusAddress
-    {
-        let a = a & 0xfe;
-        assert!(a <= 64*2 || (a >= 0x80 && a < 0xa0) || a == 0xfe);
-        BusAddress{0:a}
-    }
 
-    pub fn address(&self) ->AddressType
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum AddressError
+{
+    OK,
+    NotShort,
+    NotGroup,
+    InvalidAddress
+}
+
+impl std::fmt::Display for AddressError
+{
+    fn fmt(&self, fmt: &mut std::fmt::Formatter) 
+       -> std::result::Result<(), std::fmt::Error>
     {
-        match self.0 >> 1 {
-            a @ 0..=63 =>
-                AddressType::Short(ShortAddress::new(a+1)),
-            a @ 0x40..=0x4f => 
-                AddressType::Group(GroupAddress::new((a & 0x0f) + 1)),
-            _ => AddressType::Broadcast
+        match self {
+            AddressError::OK => write!(fmt, "OK"),
+            AddressError::NotShort => write!(fmt, "Not a short address"),
+            AddressError::NotGroup => write!(fmt, "Not a group address"),
+            AddressError::InvalidAddress => write!(fmt, "InvalidAddress")
         }
     }
 }
 
-impl std::convert::From<ShortAddress> for BusAddress
+impl std::error::Error for AddressError
 {
-    fn from(a: ShortAddress) ->Self
+}
+
+impl Address
+{
+    pub fn from_bus_address(bus: u8) -> Result<Address, AddressError>
     {
-        Self::new((a.0-1)<<1)
+        match bus >> 1 {
+            a @ 0..=63 =>
+                Ok(Address::Short(Short::new(a+1))),
+            a @ 0x40..=0x4f =>
+                Ok(Address::Group(Group::new((a & 0x0f) + 1))),
+            0xff => Ok(Address::Broadcast),
+            _ => Err(AddressError::InvalidAddress)
+        }
     }
 }
 
-impl std::convert::From<&ShortAddress> for BusAddress
+impl std::convert::From<Short> for Address
 {
-    fn from(a: &ShortAddress) ->Self
+    fn from(a: Short) ->Self
     {
-        Self::new((a.0-1)<<1)
+        Address::Short(a)
     }
 }
 
-impl std::convert::From<GroupAddress> for BusAddress
+impl std::convert::From<Group> for Address
 {
-    fn from(a: GroupAddress) ->Self
+    fn from(a: Group) ->Self
     {
-        Self::new((a.0-1)<<1 | 0x80)
+        Address::Group(a)
     }
 }
 
-impl std::convert::From<&GroupAddress> for BusAddress
+impl std::cmp::PartialEq<Short> for Address
 {
-    fn from(a: &GroupAddress) ->Self
+    fn eq(&self, other: &Short) -> bool
     {
-        Self::new((a.0-1)<<1 | 0x80)
+        match self {
+            Address::Short(a) => a == other,
+            _ => false
+        }
     }
 }
 
-impl std::convert::From<BroadcastAddress> for BusAddress
+impl std::cmp::PartialEq<Group> for Address
 {
-    fn from(a: BroadcastAddress) ->Self
+    fn eq(&self, other: &Group) -> bool
     {
-        Self::new(0xff)
+        match self {
+            Address::Group(a) => a == other,
+            _ => false
+        }
     }
 }
 
-impl std::convert::From<BusAddress> for u8
+impl BusAddress for Address
 {
-    fn from(a: BusAddress) ->Self
+    fn bus_address(&self) ->u8
     {
-        a.0
+        match(self) {
+            Address::Short(a) => a.bus_address(),
+            Address::Group(a) => a.bus_address(),
+            Address::Broadcast => 0xfe
+        }
     }
 }
-
-impl std::cmp::PartialEq<BusAddress> for BusAddress
-{
-    fn eq(&self, other: &BusAddress) -> bool
-    {
-        self.0 == other.0
-    }
-}
-
-impl std::cmp::PartialEq<ShortAddress> for BusAddress
-{
-    fn eq(&self, other: &ShortAddress) -> bool
-    {
-        self.0 == BusAddress::from(other).0
-    }
-}
-
-impl std::cmp::PartialEq<GroupAddress> for BusAddress
-{
-    fn eq(&self, other: &GroupAddress) -> bool
-    {
-        self.0 == BusAddress::from(other).0
-    }
-}
-
-
-
-
 #[derive(Debug, Copy, Clone)]
-pub struct ShortAddress(u8);
+pub struct Short(u8);
 
-impl ShortAddress {
-    pub fn new(a: u8) ->ShortAddress
+impl Short {
+    pub fn new(a: u8) ->Short
     {
         assert!(a >= 1 && a <= 64);
-        ShortAddress{0:a}
+        Short{0:a}
     }
 }
 
-impl std::convert::TryFrom<i32> for ShortAddress
+impl std::convert::TryFrom<i32> for Short
 {
-    type Error = &'static str;
+    type Error = AddressError;
     fn try_from(a: i32) ->Result<Self, Self::Error>
     {
         if a >= 1 && a <= 64 {
             Ok(Self::new(a as u8))
         } else {
-            Err("Short address out of range. 1 <= addr <= 64")
+            Err(AddressError::NotShort)
         }
     }
 }
 
-impl std::convert::TryFrom<BusAddress> for ShortAddress
+impl std::convert::TryFrom<Address> for Short
 {
-    type Error = &'static str;
-    fn try_from(a: BusAddress) ->Result<Self, Self::Error>
+    type Error = AddressError;
+    fn try_from(any: Address) ->Result<Self, Self::Error>
     {
-        if a.0 < 64 * 2 {
-            Ok(Self::new((a.0>>1)+1))
+        if let Address::Short(addr) = any {
+            Ok(addr)
         } else {
-            Err("Not a short address")
+            Err(AddressError::NotShort)
         }
     }
 }
 
-impl std::cmp::PartialEq<ShortAddress> for ShortAddress
+impl std::cmp::PartialEq<Short> for Short
 {
-    fn eq(&self, other: &ShortAddress) -> bool
+    fn eq(&self, other: &Short) -> bool
     {
         self.0 == other.0
     }
 }
 
+impl BusAddress for Short
+{
+    fn bus_address(&self) ->u8
+    {
+        (self.0 - 1) << 1
+    }
+}
     
 #[derive(Debug, Copy, Clone)]
-pub struct GroupAddress(u8);
+pub struct Group(u8);
 
-impl GroupAddress {
-    pub fn new(a: u8) ->GroupAddress
+impl Group {
+    pub fn new(a: u8) ->Group
     {
         assert!(a >= 1 && a <= 16);
-        GroupAddress{0:a}
+        Group{0:a}
     }
 }
 
-impl std::convert::TryFrom<i32> for GroupAddress
+impl std::convert::TryFrom<i32> for Group
 {
-    type Error = &'static str;
+    type Error = AddressError;
     fn try_from(a: i32) ->Result<Self, Self::Error>
     {
         if a >= 1 && a <= 16 {
             Ok(Self::new(a as u8))
         } else {
-            Err("Group address out of range. 1 <= addr <= 16")
+            Err(AddressError::NotGroup)
         }
     }
 }
 
-impl std::convert::TryFrom<BusAddress> for GroupAddress
+impl std::convert::TryFrom<Address> for Group
 {
-    type Error = &'static str;
-    fn try_from(a: BusAddress) ->Result<Self, Self::Error>
+    type Error = AddressError;
+    fn try_from(any: Address) ->Result<Self, Self::Error>
     {
-        if a.0 >= 0x80 && a.0 < 0xa0{
-            Ok(Self::new(((a.0>>1) & 0x0f) + 1))
+        if let Address::Group(addr) = any {
+            Ok(addr)
         } else {
-            Err("Not a group address")
+            Err(AddressError::NotGroup)
         }
     }
 }
 
-impl std::cmp::PartialEq<GroupAddress> for GroupAddress
+impl std::cmp::PartialEq<Group> for Group
 {
-    fn eq(&self, other: &GroupAddress) -> bool
+    fn eq(&self, other: &Group) -> bool
     {
         self.0 == other.0
     }
 }
 
-#[derive(Debug, Copy, Clone)]
-pub struct BroadcastAddress {}
-
-impl BroadcastAddress {
-    pub fn new() ->BroadcastAddress
-    {
-        BroadcastAddress{}
-    }
-}
-
-impl std::convert::TryFrom<BusAddress> for BroadcastAddress
+impl BusAddress for Group
 {
-    type Error = &'static str;
-    fn try_from(a: BusAddress) ->Result<Self, Self::Error>
+    fn bus_address(&self) ->u8
     {
-        if a.0 == 0xfe {
-            Ok(Self::new())
-        } else {
-            Err("Not a broadcast address")
-        }
+        ((self.0 - 1) << 1) | 0x80
     }
 }
 
-
-pub const BROADCAST: Address = Address{bus_addr: 0xfe};
-
-pub struct Address
-{
-    bus_addr: u8 // As it apears on the bus. Upper 7 bits used
-}
-
-pub fn device(dev: u8) -> Address
-{
-    assert!(dev >= 1 && dev <= 64);
-    Address{bus_addr: (dev-1) << 1}
-}
-    
-pub fn group(group: u8) -> Address
-{
-    assert!(group >= 1 && group <= 64);
-    Address{bus_addr: ((group - 1) << 1) | 0x80}
-}
-
-impl Address
-{
-    pub fn is_group(&self) -> bool
-    {
-        (self.bus_addr & 0xc0) == 0x80
-    }
-
-    pub fn is_device(&self) -> bool
-    {
-        (self.bus_addr & 0x80) == 0
-    }
-    
-    pub fn is_broadcast(&self) -> bool
-    {
-        self.bus_addr == 0xfe
-    }
-    
-    pub fn to_device(&self) -> Option<u8>
-    {
-        if self.is_device() {
-            Some((self.bus_addr >> 1) + 1)
-        } else {
-            None
-        }
-    }
-    pub fn to_group(&self) -> Option<u8>
-    {
-        if self.is_group() {
-            Some(((self.bus_addr & 0x3e) >> 1) + 1)
-        } else {
-            None
-        }
-    }
-
-    pub fn to_bus_addr(&self) -> u8
-    {
-        self.bus_addr
-    }
-}
+pub type Long = u32;
 
 #[test]
 fn short_address_test()
 {
-    let a:ShortAddress = 1.try_into().unwrap();
-    let b:BusAddress = a.into();
-    assert_eq!(b, ShortAddress::new(1));
-    assert_eq!(b, BusAddress::new(0x00));
+    let a:Short = 1.try_into().unwrap();
+    let b:Address = a.into();
+    assert_eq!(b, Short::new(1));
+    assert_eq!(b, Address::from_bus_address(0x00).unwrap());
     
-    let a = ShortAddress::try_from(64).unwrap();
-    let b:BusAddress = a.into();
-    assert_eq!(b, ShortAddress::new(64));
-    assert_eq!(b, BusAddress::new(0x3f<<1));
+    let a = Short::try_from(64).unwrap();
+    let b:Address = a.into();
+    assert_eq!(b, Short::new(64));
+    assert_eq!(b, Address::from_bus_address(0x3f<<1).unwrap());
     
-    let a = ShortAddress::try_from(b).unwrap();
-    assert_eq!(a, ShortAddress(64));
+    let a = Short::try_from(b).unwrap();
+    assert_eq!(a, Short(64));
 }
 
 #[test]
 fn group_address_test()
 {
-    let a:GroupAddress = 1.try_into().unwrap();
-    let b:BusAddress = a.into();
-    assert_eq!(b, GroupAddress::new(1));
-    assert_eq!(b, BusAddress::new(0x80));
+    let a:Group = 1.try_into().unwrap();
+    let b:Address = a.into();
+    assert_eq!(b, Group::new(1));
+    assert_eq!(b, Address::from_bus_address(0x80).unwrap());
     
-    let a = GroupAddress::try_from(16).unwrap();
-    let b:BusAddress = a.into();
-    assert_eq!(b, GroupAddress::new(16));
-    assert_eq!(b, BusAddress::new(0x9e));
+    let a = Group::try_from(16).unwrap();
+    let b:Address = a.into();
+    assert_eq!(b, Group::new(16));
+    assert_eq!(b, Address::from_bus_address(0x9e).unwrap());
     
-    let a = GroupAddress::try_from(b).unwrap();
-    assert_eq!(a, GroupAddress(16));
+    let a = Group::try_from(b).unwrap();
+    assert_eq!(a, Group(16));
+}
+
+#[cfg(test)]
+
+fn use_any_bus_address(bus_addr: &dyn BusAddress) -> u8
+{
+    bus_addr.bus_address()
 }
 
 #[test]
 fn bus_address_test()
 {
-    let b = BusAddress::from(ShortAddress::new(7));
-    assert_eq!(b.address(), AddressType::Short(ShortAddress::new(7)));
+    let b = Address::from(Short::new(7));
+    assert_eq!(b, Address::Short(Short::new(7)));
     
-    let b = BusAddress::from(GroupAddress::new(13));
-    assert_eq!(b.address(), AddressType::Group(GroupAddress::new(13)));
-    
-    let b = BusAddress::from(BroadcastAddress::new());
-    assert_eq!(b.address(), AddressType::Broadcast);
+    let b = Address::from(Group::new(13));
+    assert_eq!(b, Address::Group(Group::new(13)));
+    assert_eq!( use_any_bus_address(&Short::new(7)),(7-1)<<1);
+    assert_eq!( use_any_bus_address(&Group::new(12)),(12-1)<<1 | 0x80);
+    assert_eq!( use_any_bus_address(&Address::Broadcast),0xfe);
 }
+
+

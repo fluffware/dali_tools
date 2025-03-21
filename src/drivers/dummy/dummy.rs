1de4ecaf988b7e3eee2e75c955c8517358e1c135
ksb <ksb@users.sourceforge.net>
2025-03-21 19:17:27 +0100

    Added logging for dummy driver

--- src/drivers/dummy/dummy.rs
+++ src/drivers/dummy/dummy.rs
@@ -1,22 +1,58 @@
 use crate::drivers;
 use crate::utils::dyn_future::DynFuture;
 use drivers::driver::{
-    DaliBusEventResult, DaliDriver, DaliFrame, DaliSendResult,
-    DriverInfo, OpenError,
+    DaliBusEventResult, DaliDriver, DaliFrame, DaliSendResult, DriverInfo, OpenError,
 };
 use drivers::send_flags::Flags;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::Write;
 use std::pin::Pin;
+use std::sync::{Arc, Mutex};
 use std::time::{Duration, Instant};
 
-pub struct DummyDriver;
+pub struct DummyDriver {
+    log: Option<Arc<Mutex<dyn Write + Send>>>,
+}
+
 impl DaliDriver for DummyDriver {
     fn send_frame(
         &mut self,
-        _cmd: DaliFrame,
+        cmd: DaliFrame,
         flags: Flags,
     ) -> Pin<Box<dyn Future<Output = DaliSendResult> + Send>> {
+        let log = self.log.clone();
         Box::pin(async move {
+            if let Some(f) = log {
+                let mut buf = String::new();
+                if flags.send_twice() {
+                    buf += "T ";
+                } else if flags.expect_answer() {
+                    buf += "A ";
+                } else {
+		    buf += "  ";
+		}
+                match cmd {
+                    DaliFrame::Frame24(frame) => {
+                        buf += &format!("{:02x} {:02x} {:02x}", frame[0], frame[1], frame[2])
+                    }
+                    DaliFrame::Frame16(frame) => {
+                        buf += &format!("{:02x} {:02x}", frame[0], frame[1])
+                    }
+                    DaliFrame::Frame8(frame) => buf += &format!("{:02x}", frame),
+                    DaliFrame::Frame25(frame) => {
+                        buf += &format!(
+                            "{:02x} {:02x} {:02x} {:1x}",
+                            frame[0], frame[1], frame[2], frame[3]
+                        )
+                    }
+                }
+                buf += "\n";
+                let mut f = f.lock().unwrap();
+                if let Err(e) = f.write(buf.as_bytes()) {
+                    return DaliSendResult::DriverError(e.into());
+                }
+            }
             if flags.send_twice() {
                 tokio::time::sleep(Duration::from_millis(19)).await;
                 DaliSendResult::Ok
@@ -31,7 +67,7 @@ impl DaliDriver for DummyDriver {
     }
 
     fn next_bus_event(&mut self) -> DynFuture<DaliBusEventResult> {
-        Box::pin( std::future::pending() )
+        Box::pin(std::future::pending())
     }
 
     fn current_timestamp(&self) -> std::time::Instant {
@@ -43,8 +79,24 @@ impl DaliDriver for DummyDriver {
     }
 }
 
-fn driver_open(_params: HashMap<String, String>) -> Result<Box<dyn DaliDriver>, OpenError> {
-    Ok(Box::new(DummyDriver))
+fn driver_open(params: HashMap<String, String>) -> Result<Box<dyn DaliDriver>, OpenError> {
+    let mut log: Option<Arc<Mutex<dyn Write + Send>>> = None;
+    if let Some(filename) = params.get("log") {
+        if filename == "-" {
+            log = Some(Arc::new(Mutex::new(std::io::stdout())));
+        } else {
+            match File::create(filename) {
+                Ok(f) => log = Some(Arc::new(Mutex::new(f))),
+                Err(e) => {
+                    return Err(OpenError::ParameterError(format!(
+                        "Failed to log open file {}: {}",
+                        filename, e
+                    )))
+                }
+            }
+        }
+    }
+    Ok(Box::new(DummyDriver { log }))
 }
 pub fn driver_info() -> DriverInfo {
     DriverInfo {
